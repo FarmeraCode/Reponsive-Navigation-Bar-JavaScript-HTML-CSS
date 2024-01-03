e1785984c53c40fa29ec3d02b10db150167a2014
Ayesha Farrukh <farmeranetworks@gmail.com>
2024-01-03 14:10:51 +0500

    Add files via upload

---
+++ logics.js
@@ -0,0 +1,15 @@
+var menu;
+var icon;
+
+menu = document.getElementById("navlinks");
+icon = document.getElementById("cross");
+
+function showmenu(){
+    menu.style.display = 'block';
+    icon.style.display = 'block';
+}
+
+function hidemenu(){
+    menu.style.display = 'none';
+    icon.style.display = 'none';
+}
