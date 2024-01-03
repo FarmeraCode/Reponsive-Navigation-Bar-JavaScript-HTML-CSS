9d4a28faa17620bdc353476ce86920dbc73ed82d
Ayesha Farrukh <farmeranetworks@gmail.com>
2024-01-03 15:59:50 +0500

    Update logics.js

--- logics.js
+++ logics.js
@@ -1,22 +1,15 @@
 var menu;
 var icon;
-var cross;
-var bars;
 
 menu = document.getElementById("navlinks");
 icon = document.getElementById("cross");
-cross = document.getElementById("bars");
-bars = document.getElementById("cross");
 
-
-if (bars.clicked == True){
-    function showmenu(){
-        menu.style.display = 'block';
-        icon.style.display = 'block';
-    }
+function showmenu(){
+    menu.style.display = 'block';
+    icon.style.display = 'block';
 }
 
-function hidemenu(cross.clicked == True){
+function hidemenu(){
     menu.style.display = 'none';
     icon.style.display = 'none';
 }
